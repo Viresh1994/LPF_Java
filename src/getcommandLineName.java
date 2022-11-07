232c861350432ee642502e667a40a7aec0743269
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-07 18:08:32 +0530

    get name of command line arguments

---
+++ src/getcommandLineName.java
@@ -0,0 +1,8 @@
+public class getcommandLineName {
+    public static void main(String[] args) {
+        System.out.println("The command-line arguments are:\n");
+        for(int x=0; x<args.length; x++){
+            System.out.println("args[" + x + "]: " +args[ x ]);
+        }
+    }
+}
